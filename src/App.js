75f1bc7866a7c676a35ab58cf41263db29b99687
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-02 12:32:56 +0300

    Installed react-router, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { Button } from 'react-bootstrap';
+
 const App = () => {
-  return <span>App</span>;
+  return (
+    <div className='text-center'>
+      <Button>Test</Button>
+    </div>
+  );
 };
 
 export default App;
