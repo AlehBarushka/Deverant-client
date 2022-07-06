9e2c512863e204067ff7cfab3f22cb846429a3d9
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-06 15:53:41 +0300

    Fix header (#7)

---
+++ src/constants/navbar.js
@@ -0,0 +1,11 @@
+const NAV_KEYS = { statistics: '/statistics', projects: '/projects/list' };
+
+export const DEFAULT_SELCTED_ITEM = NAV_KEYS.statistics;
+
+export const NAV_ITEMS = [
+  {
+    link: NAV_KEYS.statistics,
+    title: 'Statistics',
+  },
+  { link: NAV_KEYS.projects, title: 'Projects' },
+];
