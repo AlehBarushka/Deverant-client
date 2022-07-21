3050a01a047b0f023a2862dae25a801f57f81bde
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-21 14:40:01 +0300

    Added some error handle

--- src/utils/errorHandling.js
+++ src/utils/errorHandling.js
@@ -1,3 +1,5 @@
+import { AUTH_STATUS_ERROR } from '../constants/error';
+
 /**
  * @description The function checks the status of the response from the server.
  * @param {Object} apiResponse - Response from server.
@@ -11,4 +13,12 @@ export const apiErrorHandle = apiResponse => {
   }
 
   return 'No error detected!';
+};
+
+/**
+ * @description The function return an error.
+ * @returns {Error} returns an Error with auth status error message.
+ */
+export const getAuthSatatusError = () => {
+  throw new Error(AUTH_STATUS_ERROR);
 };
