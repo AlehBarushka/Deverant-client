df56bfd36af83b983dff8c27b1c4a94e5f1322a2
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-21 14:39:30 +0300

    Added action in container and added effect

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,7 +1,12 @@
 import { connect } from 'react-redux';
+import { getAuthStatusPendingAC } from '../actionCreators/auth';
 
 import App from '../App';
 
 const mapStateToProps = state => ({ isAuthenticated: state.auth.isAuthenticated });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = {
+  getAuthStatus: getAuthStatusPendingAC,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
