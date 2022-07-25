d769645742d047b855685d6ec7063ad930ca4775
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-25 19:39:11 +0300

    Add Loader (#32)
    Added Loader

--- src/containers/ProjectListContainer.js
+++ src/containers/ProjectListContainer.js
@@ -4,7 +4,10 @@ import { getProjectsAC } from '../actionCreators/projects';
 
 import ProjectList from '../pages/ProjectList/ProjectList';
 
-const mapStateToProps = state => ({ projects: state.projectsData.projects });
+const mapStateToProps = state => ({
+  projects: state.projectsData.projects,
+  isLoading: state.application.isLoading,
+});
 
 const mapDispatchToProps = {
   getProjects: getProjectsAC,
--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,22 +1,29 @@
 import React, { useEffect } from 'react';
 
 import { Col, Container, Row } from 'react-bootstrap';
+import Loader from '../../components/Loader';
 import ProjectsCardItem from '../../components/ProjectsCardItem';
 
-const ProjectList = ({ getProjects, projects }) => {
+const ProjectList = ({ getProjects, projects, isLoading }) => {
   useEffect(() => {
     getProjects();
   }, [getProjects]);
   return (
     <Container className='mt-5 mb-3'>
-      <Row className='g-4 mb-5'>
-        {projects.length > 0 &&
-          projects.map(project => (
-            <Col key={project.id} lg={4} md={6}>
-              <ProjectsCardItem project={project} />
-            </Col>
-          ))}
-      </Row>
+      {isLoading ? (
+        <div className='d-flex justify-content-center'>
+          <Loader />
+        </div>
+      ) : (
+        <Row className='g-4 mb-5'>
+          {projects.length > 0 &&
+            projects.map(project => (
+              <Col key={project.id} lg={4} md={6}>
+                <ProjectsCardItem project={project} />
+              </Col>
+            ))}
+        </Row>
+      )}
     </Container>
   );
 };
