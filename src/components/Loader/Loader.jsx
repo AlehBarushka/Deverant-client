e0eb8d229b5d91244eda42ab7428c2025b487cb9
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-25 19:38:41 +0300

    Added Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loader = () => {
+  console.log(1);
+  return <Spinner animation='border' />;
+};
+
+export default Loader;
