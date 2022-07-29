e01e591a76408533f7b8af7e3a03e5d03b187326
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-07-29 11:07:22 +0300

    Deleted custom error handling

--- src/services/projects.js
+++ src/services/projects.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 import { BASE_URL } from '../constants/api';
 
-import { apiErrorHandle } from '../utils/errorHandling';
-
 export const projectsAPI = {
   /**
    * @description The method get all projects.
@@ -11,16 +9,8 @@ export const projectsAPI = {
    * @returns {Array} An array with objects of projects.
    */
   async getProjects(token) {
-    const { data } = await axios.get(`${BASE_URL}all_projects/${token}`);
+    const { data } = await axios.get(`${BASE_URL}all_projects/${token}?`);
 
-    //The server part returns the status code 200 if the email or password is incorrect. I had to throw the error manually
-    apiErrorHandle(data);
-
-    if (data.description === `you haven't any projects`) {
-      return [];
-    }
-
-    //crutch, data comes from the server in the form of an object with objects
-    return Object.values(data);
+    return data;
   },
 };
