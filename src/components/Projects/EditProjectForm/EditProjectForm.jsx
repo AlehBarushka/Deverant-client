f52ccb29ebe79de7fae8c9da489822183d32922b
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-08-12 16:31:52 +0300

    Added component

--- src/components/Projects/EditProjectForm/EditProjectForm.jsx
+++ src/components/Projects/EditProjectForm/EditProjectForm.jsx
@@ -2,31 +2,52 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Form, Button, InputGroup, Row, Col } from 'react-bootstrap';
-import { AiOutlineProject } from 'react-icons/ai';
 
-const EditProjectForm = ({ project }) => {
+const EditProjectForm = ({ project, updateProject }) => {
   const [editMode, setEditMode] = useState(false);
+  const [projectData, setProjectData] = useState({
+    title: project.project_name,
+    description: project.description,
+  });
+
+  const navigate = useNavigate();
 
   const onSwitchAction = () => {
     setEditMode(!editMode);
   };
 
-  const navigate = useNavigate();
-  return (
-    <Form>
-      <h2 className='text-center fs-3 mb-3 title'>
-        <AiOutlineProject className='me-1 fs-5' />
-        Project details
-      </h2>
-      <Form.Switch className='my-3' onChange={onSwitchAction} label='Edit mode' />
+  const handleOnChange = e => {
+    setProjectData({ ...projectData, [e.target.name]: e.target.value });
+  };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    updateProject({ ...projectData, id: project.id });
+  };
+
+  return (
+    <Form noValidate onSubmit={handleSubmit}>
+      <Form.Switch className='my-3' onChange={onSwitchAction} label='Edit mode' />
       <Form.Group className='mb-3'>
         <Form.Label>Project title:</Form.Label>
-        <Form.Control type='text' readOnly={!editMode} value={project.project_name} />
+        <Form.Control
+          type='text'
+          name='title'
+          readOnly={!editMode}
+          value={projectData.title}
+          onChange={handleOnChange}
+        />
       </Form.Group>
       <Form.Group className='mb-3'>
         <Form.Label>Project description:</Form.Label>
-        <Form.Control as='textarea' readOnly={!editMode} rows={3} value={project.description} />
+        <Form.Control
+          as='textarea'
+          name='description'
+          readOnly={!editMode}
+          rows={3}
+          value={projectData.description}
+          onChange={handleOnChange}
+        />
       </Form.Group>
       <Form.Group as={Row} className='mb-3'>
         <Col md={4}>
@@ -37,7 +58,7 @@ const EditProjectForm = ({ project }) => {
           </InputGroup>
         </Col>
       </Form.Group>
-      <Button disabled={!editMode} className='me-1 button-primary' onClick={() => navigate(-1)}>
+      <Button disabled={!editMode} type='submit' className='me-1 button-primary'>
         Save
       </Button>
       <Button variant='secondary' onClick={() => navigate(-1)}>
