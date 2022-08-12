4f22ca93f37f13304e1a59f37a992f3b311245c2
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-08-12 10:20:08 +0300

    Added switch & some styles, fixed displaying money

--- src/components/Projects/EditProjectForm/EditProjectForm.jsx
+++ src/components/Projects/EditProjectForm/EditProjectForm.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Form, Button, InputGroup, Row, Col } from 'react-bootstrap';
 import { AiOutlineProject } from 'react-icons/ai';
 
 const EditProjectForm = ({ project }) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const onSwitchAction = () => {
+    setEditMode(!editMode);
+  };
+
   const navigate = useNavigate();
   return (
     <Form>
@@ -12,25 +18,26 @@ const EditProjectForm = ({ project }) => {
         <AiOutlineProject className='me-1 fs-5' />
         Project details
       </h2>
+      <Form.Switch className='my-3' onChange={onSwitchAction} label='Edit mode' />
+
       <Form.Group className='mb-3'>
         <Form.Label>Project title:</Form.Label>
-        <Form.Control type='text' readOnly value={project.project_name} />
+        <Form.Control type='text' readOnly={!editMode} value={project.project_name} />
       </Form.Group>
       <Form.Group className='mb-3'>
         <Form.Label>Project description:</Form.Label>
-        <Form.Control as='textarea' readOnly rows={3} value={project.description} />
+        <Form.Control as='textarea' readOnly={!editMode} rows={3} value={project.description} />
       </Form.Group>
       <Form.Group as={Row} className='mb-3'>
         <Col md={4}>
           <Form.Label>Cost:</Form.Label>
           <InputGroup className='mb-2'>
             <InputGroup.Text>$</InputGroup.Text>
-            <Form.Control type='number' readOnly value={project.separately_money} />
+            <Form.Control type='number' readOnly={!editMode} value={project.money} />
           </InputGroup>
         </Col>
       </Form.Group>
-
-      <Button disabled className='me-1 button-primary' onClick={() => navigate(-1)}>
+      <Button disabled={!editMode} className='me-1 button-primary' onClick={() => navigate(-1)}>
         Save
       </Button>
       <Button variant='secondary' onClick={() => navigate(-1)}>
